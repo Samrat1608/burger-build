24d2b6a70590e077875756600454a8f0d801e6e3
shubham <shubham.ingole@neosofttech.com>
2020-09-18 18:09:45 +0530

    burger static

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from "./containers/BurgerBulider/BurgerBuilder";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
