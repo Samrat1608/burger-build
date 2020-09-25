367f52cdb7f64aee3e8b0483538843e606c7db89
shubham <shubham.ingole@neosofttech.com>
2020-09-25 17:03:57 +0530

    redux

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'https://react-my-burger-7622a.firebaseio.com/'
-});
+    baseURL: 'https://react-my-burger-7622a.firebaseio.com/'});
 
 export default instance;
