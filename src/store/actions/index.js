100dc9a271c96411c683b2774299d63a44074e85
shubham <shubham.ingole@neosofttech.com>
2020-10-12 13:45:25 +0530

    redux saga

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,5 @@
-export {addIngredient, removeIngredient, initIngredients} from './burgerBuilder';
+export {addIngredient, removeIngredient, initIngredients, setIngredients, fetchIngredientsFailed} from './burgerBuilder';
 
-export {purchaseBurgerStart,purchaseBurger, purchaseInit, fetchOrders} from './order';
+export { purchaseBurgerStart,purchaseBurger, purchaseInit, fetchOrders, purchaseBurgerFail, purchaseBurgerSuccess, fetchOrdersStart, fetchOrdersSuccess, fetchOrdersFail } from './order';
 
-export { auth,logout, setAuthRedirectPath, authCheckState } from './auth';
+export { auth,logout, setAuthRedirectPath, authCheckState, logoutSucced, authStart, authSuccess, authFail,checkAuthTimeout } from './auth';
